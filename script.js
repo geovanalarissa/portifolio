e907a33c9179c36bf7a9cd2cadeee3b3ce92c1cf
Geovana Larissa <geovanalarissa09@icloud.com>
2026-04-25 22:42:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.getElementById("meuForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const nome = document.getElementById("nome").value.trim();
+        const telefone = document.getElementById("telefone").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const mensagem = document.getElementById("mensagem");
+
+        if (nome === "" || telefone === "" || email === "") {
+            alert("Por favor, preencha todos os campos!");
+            return;
+        }
+
+        const regexTelefone = /^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$/;
+        if (!regexTelefone.test(telefone)) {
+            alert("Digite um telefone válido. Ex: (81) 99999-9999");
+            return;
+        }
+
+        const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!regexEmail.test(email)) {
+            alert("Digite um email válido.");
+            return;
+        }
+
+        mensagem.textContent = "Formulário enviado com sucesso!";
+        mensagem.style.color = "green";
+    });
+
+});
